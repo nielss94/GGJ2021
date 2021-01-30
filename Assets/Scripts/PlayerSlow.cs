de00c7fd3c1212f23d4fdc59c9c42c1b8a97bb28
Chris <christian-boer@hotmail.com>
2021-01-30 23:44:47 +0100

    Ballenbak erin

--- Assets/Scripts/PlayerSlow.cs
+++ Assets/Scripts/PlayerSlow.cs
@@ -9,11 +9,13 @@ public class PlayerSlow : MonoBehaviour {
     public float slowAmount = 0.5f;
 
     private BaseFirstPersonController controller;
+    private bool isSlowed = false;
     private float initialForwardSpeed;
     private float initialBackwardSpeed;
     private float initialStrafeSpeed;
     
     private void Start() {
+        PlayerDash.OnStopDash += CheckSlow;
         controller = GetComponent<BaseFirstPersonController>();
         initialForwardSpeed = controller.forwardSpeed;
         initialBackwardSpeed = controller.backwardSpeed;
@@ -21,14 +23,24 @@ public class PlayerSlow : MonoBehaviour {
     }
 
     public void EnableSlow() {
+        isSlowed = true;
         controller.forwardSpeed = initialForwardSpeed * slowAmount;
         controller.backwardSpeed = initialBackwardSpeed * slowAmount;
         controller.strafeSpeed = initialStrafeSpeed * slowAmount;
     }
 
     public void DisableSlow() {
+        isSlowed = false;
         controller.forwardSpeed = initialForwardSpeed;
         controller.backwardSpeed = initialBackwardSpeed;
         controller.strafeSpeed = initialStrafeSpeed;
+    }
+
+    private void CheckSlow() {
+        if (isSlowed) {
+            controller.forwardSpeed = initialForwardSpeed * slowAmount;
+            controller.backwardSpeed = initialBackwardSpeed * slowAmount;
+            controller.strafeSpeed = initialStrafeSpeed * slowAmount;
+        }
     }
 }
