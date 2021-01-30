560255dbad8d42a619e48e0032e8d2dbb33b7a99
Niels van Dam <nielsvdam@hotmail.com>
2021-01-30 22:42:27 +0100

    uinique childrengaa!

--- Assets/Scripts/Child.cs
+++ Assets/Scripts/Child.cs
@@ -6,11 +6,23 @@ using UnityEngine;
 
 public class Child : MonoBehaviour
 {
-    public float knockdownTime;
     public bool canGetKnockedDown = true;
-
+    public Transform hats;
+    [SerializeField] private Renderer renderer;
+    
     public void KnockBack(Vector3 normalizedAngle, float force)
     {
         GetComponent<Rigidbody>().AddForce(normalizedAngle * force, ForceMode.Impulse);
+    }
+
+    public void SetVisualCombination(KeyValuePair<Material, string> combo)
+    {
+        renderer.material = combo.Key;
+        
+        // Set hat
+        if (combo.Value != "")
+        {
+            hats.Find(combo.Value).gameObject.SetActive(true);
+        }
     }
 }
