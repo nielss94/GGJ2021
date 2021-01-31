557a3abc25e36fd0b60147f457716f72f97b7709
Bart in 't Veld <bart.int.007@gmail.com>
2021-01-31 12:12:50 +0100

    Score menu styled

---
+++ Assets/Scripts/ScoreMenu.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
