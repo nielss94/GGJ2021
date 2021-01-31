2eb51573832312f1359eb71ae9c53dbe1ad166c6
Niels van Dam <nielsvdam@hotmail.com>
2021-01-31 12:34:39 +0100

    intro dialog!

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialog
+{
+    public Sprite image;
+    public AudioClip audioClip;
+    public string text;
+}
