d6d04f3561642803e923b8aae8d1dadf59f5bbe7
Niels van Dam <nielsvdam@hotmail.com>
2021-01-31 17:07:08 +0100

    timescale

---
+++ Assets/Scripts/TimeScale.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScale : MonoBehaviour
+{
+    private void Awake()
+    {
+        Time.timeScale = 1;
+    }
+}
